d62bde09aa6102ba18ef2905cdf950c6a0b8dd26
LasyaSai <lasyaasaii@gmail.com>
2026-05-17 11:35:10 +0400

    feat:Package.json files added

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/index.ts'],
+  coverageThreshold: {
+    global: { branches: 70, functions: 70, lines: 70, statements: 70 },
+  },
+}
